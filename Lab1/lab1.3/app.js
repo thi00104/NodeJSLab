d60208dbb3ba53779d25d13c1d1d8b50ebeced64
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-07 10:45:08 +0700

    lab1.3 send response

---
+++ Lab1/lab1.3/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+});
+
+server.listen(3000);
