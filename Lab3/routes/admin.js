98a2c5936b7f2760e578c4494cbc0e09b1dc199d
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-09 18:06:57 +0700

    lab3.4 convert HTML to Pug

--- Lab3/routes/admin.js
+++ Lab3/routes/admin.js
@@ -9,11 +9,10 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
 router.post("/add-product", (req, res, next) => {
-  // console.log(req.body);
   products.push({ title: req.body.title });
   res.redirect("/");
 });
