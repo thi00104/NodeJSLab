39f7a96a5c7d16d5a043056ffdf019a4505d64e1
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-09 17:46:17 +0700

    lab3.3 output dynamic content

--- Lab3/routes/shop.js
+++ Lab3/routes/shop.js
@@ -8,9 +8,8 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // console.log("shop.js", adminData.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
