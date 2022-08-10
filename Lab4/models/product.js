806f84fbc0bbaf9858be6a594bf48863a64165fe
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-10 22:16:06 +0700

    lab4.4 store data in files via the model

--- Lab4/models/product.js
+++ Lab4/models/product.js
@@ -1,15 +1,33 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename, "data", "product.json")
+);
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
