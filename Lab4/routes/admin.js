59e592cc570d81f81b79c0841a95ed0589786fc1
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-10 10:49:31 +0700

    lab4.1 add controllers

---
+++ Lab4/routes/admin.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const path = require("path");
+
+const productsController = require("../controllers/products");
+
+const router = express.Router();
+
+router.get("/add-product", productsController.getAddProduct);
+
+router.post("/add-product", productsController.postAddProduct);
+
+exports.routes = router;
