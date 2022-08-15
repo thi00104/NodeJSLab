b63bed6f716f266bec8596a52d8455dba1631374
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-15 22:46:51 +0700

    lab4.10 render the product detail view

--- Lab4/controllers/shop.js
+++ Lab4/controllers/shop.js
@@ -18,6 +18,17 @@ exports.getProduct = (req, res, next) => {
   res.redirect("/");
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
