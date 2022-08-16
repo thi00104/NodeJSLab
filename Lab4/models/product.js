f722eec446de5f185e3cf7a4c9b52fe83fe993d7
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-16 23:08:56 +0700

    lab4.17 add the product delete functionality

--- Lab4/models/product.js
+++ Lab4/models/product.js
@@ -47,13 +47,23 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const updatedProducts = products.filter((prod) => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
 
   static findById(id, cb) {
     getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id);
+      const product = products.find((prod) => prod.id === id);
       cb(product);
     });
   }
