0dd47260c2d1c10e8838fa8d46adfce2f184e83e
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-17 17:15:59 +0700

    lab4.20 delete items from cart

--- Lab4/models/product.js
+++ Lab4/models/product.js
@@ -67,7 +67,10 @@ module.exports = class Product {
 
   static findById(id, cb) {
     getProductsFromFile((products) => {
+      // console.log(typeof id);
+      // console.log(products);
       const product = products.find((prod) => prod.id === id);
+      // console.log(product);
       cb(product);
     });
   }
