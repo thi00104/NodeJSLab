fc7ed6b9f14b6ac37d52963c84ccd5febc56cb1a
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-17 23:43:28 +0700

    lab5.7 fetch a single product

--- Lab5/models/product.js
+++ Lab5/models/product.js
@@ -34,13 +34,7 @@ module.exports = class Product {
     return db.execute("SELECT * FROM products");
   }
 
-  static findById(id, cb) {
-    // getProductsFromFile((products) => {
-    //   // console.log(typeof id);
-    //   // console.log(products);
-    //   const product = products.find((prod) => prod.id === id);
-    //   // console.log(product);
-    //   cb(product);
-    // });
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE id = ?", [id]);
   }
 };
