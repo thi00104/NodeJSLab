5491f3cd827d2343f18f6d9887fed4b3f09e96ee
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-18 00:33:03 +0700

    lab5.9 define a model

--- Lab5/util/database.js
+++ Lab5/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node-complete", "Truc@2022", {
+const sequelize = new Sequelize("node-complete", "root", "Truc@2022", {
   dialect: "mysql",
   host: "localhost",
 });
