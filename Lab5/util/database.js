7c210102a6e1e16b30e1baed308188ae02f38512
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-08-18 00:24:18 +0700

    lab5.8 connect to the database

--- Lab5/util/database.js
+++ Lab5/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "Truc@2022", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Truc@2022",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
