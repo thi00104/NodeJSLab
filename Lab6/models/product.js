76cf1c9d448acbb05e3352ec8dc3957c3542b266
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-09-17 23:37:29 +0700

    lab6.7 fetch a single product

--- Lab6/models/product.js
+++ Lab6/models/product.js
@@ -1,4 +1,6 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
+
 class Product {
   constructor(title, price, description, imageUrl) {
     this.title = title;
@@ -28,6 +30,37 @@ class Product {
       .then((products) => {
         console.log(products);
         return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // static findById(prodId) {
+  //   console.log(prodId.length);
+  //   const db = getDb();
+  //   return db
+  //     .collection("products")
+  //     .find({ _id: "6325ee8bdc36946e68559562" })
+  //     .next()
+  //     .then((product) => {
+  //       console.log(product);
+  //       return product;
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
       })
       .catch((err) => {
         console.log(err);
