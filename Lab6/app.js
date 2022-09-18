de930dd52b4dc7149fb72c07fd3e566876d0822e
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-09-18 10:55:32 +0700

    lab6.14 create new users

--- Lab6/app.js
+++ Lab6/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     // console.log(user);
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
+  User.findById("632695de08752692c978348c")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
--- Lab6/models/user.js
+++ Lab6/models/user.js
@@ -1,16 +1,22 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const db = require("../util/database").getDb;
 
-const sequelize = require("../util/database");
+const ObjectId = mongodb.ObjectId;
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({ _id: ObjectId(userId).next() });
+  }
+}
 
 module.exports = User;
