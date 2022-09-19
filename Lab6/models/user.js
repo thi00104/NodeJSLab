e48234acff66bd5be6989e4dc0c458f236c5f4d2
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-09-19 17:08:45 +0700

    lab6.16 work on cart items & orders

--- Lab6/models/user.js
+++ Lab6/models/user.js
@@ -3,7 +3,7 @@ const getDb = require("../util/database").getDb;
 
 const ObjectId = mongodb.ObjectId;
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.name = username;
     this.email = email;
   }
@@ -11,6 +11,20 @@ class User {
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
