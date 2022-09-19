30eb5473dfcc812cc874be2f33d82e6725f3aa51
Nguyen Anh Thi <nguyenanhthi@Macbooks-MacBook-Pro.local>
2022-09-19 17:29:41 +0700

    lab6.17 add the 'Add to Cart' functionality

--- Lab6/app.js
+++ Lab6/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("632695de08752692c978348c")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.name, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
